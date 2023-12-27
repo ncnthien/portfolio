d02b98006704df6dd147b7140d164ddb02389af4
thien.nguyen <thien.nguyen@admins-MacBook-Pro-4.local>
2023-12-28 02:57:10 +1100

    master: update work and education

--- src/pages/content.js
+++ src/pages/content.js
@@ -7,11 +7,55 @@ export const website = "https://github.com/ncnthien";
 export const about =
   "I am Nguyen Cong Nhat Thien, a developer loves making funny stuffs, learning new programming things, and trying to do better.";
 
-export const work = [];
+export const work = [
+  {
+    role: 'Frontend Developer',
+    from: '02/2021',
+    to: '07/2021',
+    location: 'Working as a fresher developer, build UI from designs and start using more technologies...',
+    skills: 'HTML, CSS, JS, ReactJS...',
+    org_url: 'https://tekup.vn/en/home/',
+    org: 'Tekup'
+  },
+  {
+    role: 'Frontend Developer',
+    from: '08/2021',
+    to: '06/2022',
+    location: 'Working as a front-end developer, join meetings, receive and analyze requirements from customers, mentor new freshers...',
+    skills: 'ReactJS, Typescript, Mobx, Redux...',
+    org_url: 'https://stdio.asia/',
+    org: 'Stdio'
+  },
+  {
+    role: 'Frontend Developer',
+    from: '06/2022',
+    to: 'Present',
+    location: 'Working as a front-end developer, meetings with clients, working in Scrum process, sharing and contribute in functional team...',
+    skills: 'Graphql, Apollo, build tools...',
+    org_url: 'https://enouvo.com/',
+    org: 'Enouvo'
+  }
+
+];
 
 export const projects = [];
 
-export const education = [];
+export const educations = [
+  {
+    course: "High School",
+    from: "2015",
+    to: "2018",
+    location: "Phan Chau Trinh High School",
+    cgpa: "7.5",
+  },
+  {
+    course: "Bachelor of Software Engineering",
+    from: "2018",
+    to: "2022",
+    location: "Duy Tan University",
+    cgpa: "3.0",
+  },
+];
 
 export const contact = [
   {
