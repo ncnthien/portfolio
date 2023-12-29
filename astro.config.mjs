d2ddca2ea420cb2c2e37927ff55d835521d9f3c0
thien.nguyen <thien.nguyen@admins-MacBook-Pro-4.local>
2023-12-30 03:19:35 +1100

    master: remove alpinejs and add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
-import alpinejs from "@astrojs/alpinejs";
+import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
-  output: 'server',
+  integrations: [tailwind(), react()],
+  output: "server",
   adapter: vercel({
     analytics: true,
   }),
 });
+
